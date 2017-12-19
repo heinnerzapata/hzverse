c13911a8274b0e15de8798945cfcaa18e9301314
Your Name <you@example.com>
2017-12-19 05:09:51 -0500

    adding some unit test

--- hzverse-db/tests/agent-test.js
+++ hzverse-db/tests/agent-test.js
@@ -18,6 +18,13 @@ let id = 1
 let AgentStub = null
 let db = null
 let sandbox = null
+let uuid = 'yyy-yyy-yyy'
+
+let uuidArgs = {
+  where: {
+    uuid: uuid
+  }
+}
 
 test.beforeEach(async () => {
   sandbox = sinon.sandbox.create()
@@ -25,6 +32,18 @@ test.beforeEach(async () => {
   AgentStub = {
     hasMany: sandbox.spy()
   }
+
+  // Model create Stub
+  AgentStub.findOne = sandbox.stub()
+  AgentStub.findOne.withArgs(uuidArgs).returns(Promise.resolve(agentFixtures.byUuid(uuid)))
+
+  // MOdel Update Stub
+  AgentStub.update = sandbox.stub()
+  AgentStub.update.withArgs(single, uuidArgs).returns(Promise.resolve(single))
+
+  // Model findById setupDatabase
+  AgentStub.findById = sandbox.stub()
+  AgentStub.findById.withArgs(id).returns(Promise.resolve(agentFixtures.byId(id)))
 
   const setupDatabase = proxyquire('../', {
     './models/agent': () => AgentStub,
@@ -52,5 +71,19 @@ test.serial('Setup', t => {
 test.serial('Agent#findById', async t => {
   let agent = await db.Agent.findById(id)
 
+  t.true(AgentStub.findById.called, 'FindById should be called on model')
+  t.true(AgentStub.findById.calledOnce, 'findById should be called one')
+  t.true(AgentStub.findById.calledWith(id), 'findById should be called with specified id')
+
   t.deepEqual(agent, agentFixtures.byId(id), 'Shoud be the same')
+})
+
+test.serial('Agent#createOrUpdate - existe', async t => {
+  let agent = await db.Agent.createOrUpdate(single)
+
+  t.true(AgentStub.findOne.called, 'findOne should be called on model')
+  t.true(AgentStub.findOne.calledTwice, 'findOne should be called twice')
+  t.true(AgentStub.update.calledOnce, 'update should be called once')
+
+  t.deepEqual(agent, single, 'agent should be the same')
 })
